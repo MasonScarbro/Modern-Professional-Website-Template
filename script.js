42d2b50ade1333fd35eb8dbad5a72118d31baa18
Mason Scarbro <masonscarbropf@gmail.com>
2023-05-24 02:23:53 -0400

    Updated Website to have buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+const link1 = document.querySelector('.link1');
+link1.addEventListener('click', () => {
+    window.open("INSERT-HERE"); // <-- Paste Link INSIDE ("") BOTH PARENTHESES AND QOUTES
+});
+// COPY ONE OF THESE TO CREATE NEW LINK PASTE AND CHANGE TO APPROPIATE link #
+const link2 = document.querySelector(".link2");
+link2.addEventListener('click', () => {
+    window.open("INSERT-HERE");
+});
